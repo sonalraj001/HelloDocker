09e6eab06ae4b98537d1e516741e6d07aa0a309a
sonal <rajsonal321@gmail.com>
2026-06-10 23:32:14 +0530

    db connection done

--- src/main/java/com/example/Dockerdemo/HelloController.java
+++ src/main/java/com/example/Dockerdemo/HelloController.java
@@ -1,8 +1,11 @@
 package com.example.Dockerdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class HelloController {
@@ -11,5 +14,13 @@ public class HelloController {
     public String greet(){
         System.out.println("hello docker");
         return "hello docker";
+    }
+
+    @Autowired
+    StudentRepo studentRepo;
+
+    @RequestMapping("/getStudents")
+    public List<Student> getStudents(){
+        return studentRepo.findAll();
     }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO student (name,age) VALUES ('test1',22);
+INSERT INTO student (name,age) VALUES ('test2',23);
+INSERT INTO student (name,age) VALUES ('test3',21);
+INSERT INTO student (name,age) VALUES ('test4',25);
